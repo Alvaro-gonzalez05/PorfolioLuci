b274e474b5858e5174461f3f18972342f24bde47
xAlva69 <as.gonzalez@alumno.etec.um.edu.ar>
2024-07-12 00:28:19 -0300

    Listo

--- src/components/Body/body.jsx
+++ src/components/Body/body.jsx
@@ -1,4 +1,3 @@
-import {Link} from 'react-router-dom';
 import * as React from 'react';
 import './body.css'
 import logo from '../../multimedia/Luci.png';
--- src/components/Body2/Body2.jsx
+++ src/components/Body2/Body2.jsx
@@ -1,4 +1,3 @@
-import {Link} from 'react-router-dom';
 import * as React from 'react';
 import './Body2.css'
 import instagram from '../../multimedia/instagramlogo.png'
